e1bb6a6f77151c14089f9c86a0efbd3589840a10
Rohan Prinja <1688456+ajnirp@users.noreply.github.com>
2020-05-18 03:08:24 -0700

    Update mandelbrot.glsl

--- mandelbrot.glsl
+++ mandelbrot.glsl
@@ -1,21 +1,23 @@
-// Useful resource: https://www.codingame.com/playgrounds/2358/how-to-plot-the-mandelbrot-set/mandelbrot-set
-// https://www.shadertoy.com/view/WdBfDR
-
 // Multiply two complex numbers.
 vec2 mult(vec2 a, vec2 b) {
-    return vec2(a.x*b.x-a.y*b.y, a.y*b.x+a.x*b.y);
+	return vec2(a.x*b.x-a.y*b.y, a.y*b.x+a.x*b.y);
 }
 
 // Return # iterations it takes for the function to diverge.
 // If it doesn't diverge return max_iter.
-int iter(vec2 c, const int max_iter, const float divergence_threshold) {
-    vec2 z = vec2(0.);
+float iter(vec2 c, const int max_iter, const float divergence_threshold) {
+	vec2 z = vec2(0.);
     int n = 0;
     while (length(z) <= 2. && n < max_iter) {
-        z = mult(z,z) + c;
+    	z = mult(z,z) + c;
         n++;
     }
-    return n;
+    // Adapted from https://stackoverflow.com/a/1243788, but
+    // I threw in another log because why not. The results are
+    // entirely different than with just 2 log's.
+    float n_smooth = float(n)+1.-log2(log2(log2(length(z))));
+    // We take the 5th root to get a milder and smoother range of color.
+    return pow(n_smooth/float(max_iter), 0.2);
 }
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
@@ -23,8 +25,6 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     vec2 uv = 2.*fragCoord/iResolution.y-vec2(2.5,1.);
     const int max_iter = 500;
     const float divergence_threshold = 2.;
-    int n = iter(uv, max_iter, divergence_threshold);
-    // We take the 4th root to get a milder and smoother range of color.
-    float col = pow(float(n)/float(max_iter), 0.25);
+    float col = iter(uv, max_iter, divergence_threshold);
     fragColor = vec4(1.-col);
 }
